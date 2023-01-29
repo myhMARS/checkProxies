1bec17b73a5c31e9b1a5baa55e835f85e218d065
mengyonghao <1533512157@qq.com>
2023-01-29 13:07:57 +0800

    更新模糊国家搜索，自动修改

--- choose_country.py
+++ choose_country.py
@@ -1,8 +1,11 @@
+import difflib
+import os
+import re
+import sys
+import time
+
 import requests
 from lxml import etree
-import os
-import sys
-import difflib
 
 
 def main():
@@ -21,13 +24,29 @@ def main():
     urls = e.xpath("//body/div[4]/div/div/ul/li/a/@href")
     countries = e.xpath("//body/div[4]/div/div/ul/li/a//text()")
     print("获取成功")
-    choose = input("选择国家方式:\n1.模糊搜索（未完成）\n2.列表选取\nchoose:")
+    choose = input("选择国家方式:\n1.模糊搜索\n2.列表选取\nchoose:")
+    # 模糊搜索
     if choose == '1':
-        # TODO: 模糊搜索demo(未完成)
-        os.system('cls')
-        country = input("输入国名(英语):")
-        res = difflib.get_close_matches(country, countries, 4, cutoff=0.6)
-        print(res)
+        while True:
+            os.system('cls')
+            country = input("输入国名(英语):")
+            res = difflib.get_close_matches(country, countries, 10, cutoff=0.55)
+            if len(res) == 0:
+                continue
+            for i in range(len(res)):
+                print(f"{i+1}.{res[i]}")
+            try:
+                choose = int(input("choose:"))
+            except ValueError:
+                print("非法输入")
+                time.sleep(1)
+                continue
+            x = countries.index(str(res[choose-1]))
+            flag = True
+            if countries[x] != "China":
+                flag = False
+            write_auto(str(urls[x]), flag)
+            break
     elif choose == '2':
         os.system('cls')
         for i in range(len(countries)):
@@ -44,13 +63,26 @@ def main():
                     break
                 elif x.isnumeric() and len(countries) >= int(x) > 0:
                     os.system('cls')
-                    print("请手动修改main.py中setting函数中country值为'"+urls[i]+"'")
+                    flag = True
                     if countries[i] != "China":
-                        print("ip_flag = False")
+                        flag = False
+                    write_auto(str(urls[i]), flag)
                     break
                 else:
                     print('非法输入')
                     break
+
+
+# 实现对main.py文件的自动修改
+def write_auto(country, flag):
+    f = open('main.py', 'r', encoding='utf-8')
+    a = f.read()
+    s1 = re.sub(r"country = .+", f"country = '{country}'", a, 1)
+    s2 = re.sub(r"ip_flag = .+", f"ip_flag = {flag}", s1, 1)
+    f = open('main.py', 'w', encoding='utf-8')
+    f.write(s2)
+    f.close()
+    print("写入完成")
 
 
 if __name__ == "__main__":
